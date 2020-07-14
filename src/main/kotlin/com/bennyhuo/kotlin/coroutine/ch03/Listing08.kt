51cd1524c6358d2cdc2a04d705a40ac4124fc7ec
wangshengxing <www.sheng.xing@gmail.com>
2020-07-14 11:00:08 +0800

    init project

---
+++ src/main/kotlin/com/bennyhuo/kotlin/coroutine/ch03/Listing08.kt
@@ -0,0 +1,19 @@
+package com.bennyhuo.kotlin.coroutine.ch03
+
+import kotlin.concurrent.thread
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun notSuspend() = suspendCoroutine<Int> { continuation ->
+//    continuation.resume(100)
+    thread {
+        Thread.sleep(100)
+        continuation.resume(100)
+    }
+
+}
+
+suspend fun main(){
+    val result=notSuspend()
+    println("notSuspend end ${result}")
+}
---
+++ src/main/kotlin/com/bennyhuo/kotlin/coroutine/ch03/Listing10.kt
@@ -0,0 +1,18 @@
+package com.bennyhuo.kotlin.coroutine.ch03
+
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun main() {
+    val ref = ::notSuspend
+//    ref.call()
+    val result = ref.call(object: Continuation<Int> {
+        override val context = EmptyCoroutineContext
+
+        override fun resumeWith(result: Result<Int>) {
+            println("resumeWith: ${result.getOrNull()}")
+        }
+    })
+
+    println(result)
+}
