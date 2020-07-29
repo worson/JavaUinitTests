6a86070c5eb39d0845c8818bfe03643855ec1134
wangshengxing <www.sheng.xing@gmail.com>
2020-07-29 10:50:14 +0800

    feature: test local proxy ok

---
+++ src/main/kotlin/com/test/http/TestProxyRequest.kt
@@ -0,0 +1,37 @@
+package com.test.http
+
+import com.langogo.transcribe.comm.L
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.junit.Assert
+import org.junit.Test
+import java.net.InetSocketAddress
+import java.net.Proxy
+
+
+/**
+ * 说明:
+ * @author wangshengxing  07.26 2020
+ */
+class TestProxyRequest {
+    val  TAG = "TestProxyRequest"
+    @Test
+    fun sampleHttpRequest(){
+        val client=OkHttpClient()
+        val request=Request.Builder().url("https://www.google.com.hk/").build()
+        val response=client.newCall(request).execute()
+        Assert.assertEquals(200,response.code())
+        L.i(TAG, { "sampleHttpRequest: ${response.body()?.string()}" })
+    }
+
+    @Test
+    fun sampleProxyHttpRequest(){
+        val client=OkHttpClient.Builder()
+            .proxy(Proxy(Proxy.Type.HTTP, InetSocketAddress("127.0.0.1", 1235)))
+            .build()
+        val request=Request.Builder().url("https://www.google.com.hk/").build()
+        val response=client.newCall(request).execute()
+        Assert.assertEquals(200,response.code())
+        L.i(TAG, { "sampleHttpRequest: ${response.body()?.string()}" })
+    }
+}
